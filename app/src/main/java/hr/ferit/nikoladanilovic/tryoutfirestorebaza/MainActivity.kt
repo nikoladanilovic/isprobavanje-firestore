f1c9d57708b46b5143b4ea6ffdfb01486fdb5a83
nikoladanilovic <nikola.danilovic28@gmail.com>
2021-07-25 18:45:05 +0200

    retrieval of multiple documents and queries

--- app/src/main/java/hr/ferit/nikoladanilovic/tryoutfirestorebaza/MainActivity.kt
+++ app/src/main/java/hr/ferit/nikoladanilovic/tryoutfirestorebaza/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private val KEY_DESCRIPTION = "description"
 
     private val db : FirebaseFirestore = FirebaseFirestore.getInstance()
-    private val noteRef : DocumentReference = db.collection("Notebook").document("My first Note")    //Moze se napisati i db.document("Notebook/My First Note") itd..
+    private val noteBookRef : CollectionReference = db.collection("Notebook")
     private lateinit var noteListener : ListenerRegistration
 
     private lateinit var  mainBinding: ActivityMainBinding
@@ -22,11 +22,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        mainBinding.buttonSave.setOnClickListener { saveData() }
+        mainBinding.buttonSave.setOnClickListener { addData() }
         mainBinding.buttonLoad.setOnClickListener { loadData() }
-        mainBinding.buttonUpdate.setOnClickListener { updateDescription() }
-        mainBinding.buttonDeleteDesc.setOnClickListener { deleteDescription() }
-        mainBinding.buttonDeleteNote.setOnClickListener { deleteNote() }
         setContentView(mainBinding.root)
     }
 
@@ -35,27 +32,37 @@ class MainActivity : AppCompatActivity() {
     //U onStart metodi se treba implementirati SNAPSHOT LISTENER
     override fun onStart() {
         super.onStart()
-        noteListener = noteRef.addSnapshotListener { snapshot, e ->
-            if(e != null) {
+
+
+        noteListener = noteBookRef.addSnapshotListener { value, e ->    //Ako se zeli primjeniti query , onda se pise: noteBookRef.whereEqualTo("title", "nekinaslov").addSnapshotListener { ..sve isto.. }
+            var data = ""
+
+            if (e != null) {
                 Log.w(TAG, "Listen failed.", e)
                 return@addSnapshotListener
             }
 
-            if(snapshot != null && snapshot.exists()){
-                //Log.d(TAG, "Current data: ${snapshot.data}")
-                //mainBinding.tvNoteContent.text = "Title: " + snapshot.getString(KEY_TITLE) + "\n" + "Description: " + snapshot.getString(KEY_DESCRIPTION)
-
-                //Koristenjem CUSTOM OBJECTA
-                val note = snapshot.toObject(
-                    Note::class.java
-                )
-
-                mainBinding.tvNoteContent.text = "Title: " + note?.title + "\n" + "Description: " + note?.description
-
-            } else {
-                Log.d(TAG, "Current data: null")
-                mainBinding.tvNoteContent.text = ""
+            /*
+            val cities = ArrayList<String>()
+            for (doc in value!!) {
+                doc.getString("name")?.let {
+                    cities.add(it)
+                }
             }
+
+             */
+
+            for (doc in value!!) {
+                val note = doc.toObject(Note::class.java)
+                note.setDocumentId(doc.id)
+
+
+                data += "ID: " + note.getDocumentId() + "\nTitle: " + note.getTitle() + "\n" + "Description: " + note.getDescription() + "\n\n"
+            }
+
+            mainBinding.tvNoteContent.text = data
+
+
         }
     }
 
@@ -66,83 +73,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
-        noteRef.get()
-            .addOnSuccessListener { document ->
-                if(document != null){
-                    //Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                    // u javi se moze jos i ovako napraviti.. Map<String, Object> note = documentSnapshot.getData();
-                    //mainBinding.tvNoteContent.text = document.getString(KEY_TITLE)  //dohvacanje po klucu, odnosno nazivu svojstva
+        var data = ""
 
-                    //Koristenjem CUSTOM DATA OBJECT-a
-                    val note = document.toObject(
-                        Note::class.java
-                    )
+        noteBookRef.get()   //ovime se dohvacaju svi dokumenti iz kolekcije, ako se hoce radit QUERY, onda se pise noteBookRef.whereEqualTo("capital", true).get().addOnSuccess...
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    //tu bi se trebalo dodavati pojedine dokumente u polje elemenata tog tipa, u ovom slucaju tipa Note
+                    Log.d(TAG, "${document.id} => ${document.data}")
 
-                    mainBinding.tvNoteContent.text = "Title: " + note?.title + "\n" + "Description: " + note?.description
+                    val note = document.toObject(Note::class.java)
+                    note.setDocumentId(document.id)
 
-                } else {
-                    Log.d(TAG, "No such document")
+                    //note.setDocumentId(document.id) //treba se napraviti setDocumentId metoda u data klasi Note i treba se stvoriti taj setter (a i getter treba ako su clanovi private)
+                    //kod getera (getDocumentId()) treba staviti @Exclude
+
+                    data += "ID: " + note.getDocumentId() + "\nTitle: " + note.getTitle() + "\n" + "Description: " + note.getDescription() + "\n\n"
                 }
+                mainBinding.tvNoteContent.text = data
+
+                //noteBookRef.document(documentId) // PRISTUP DOKUMENTU PREKO NJEGOVOG ID-A
             }
             .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
+                Log.d(TAG, "Error getting documents: ", exception)
+                mainBinding.tvNoteContent.text = ""
             }
     }
 
-    private fun saveData() {
+    private fun addData() {
         val title = mainBinding.editTextTitle.text.toString()
         val description = mainBinding.editTextDescription.text.toString()
 
-        //val note: MutableMap<String, Any> = hashMapOf()    //ovdje se koristi HASH MAP
-        //note.put(KEY_TITLE, title)
-        //note.put(KEY_DESCRIPTION, description)
-        // mogu se proizvoljno dodavat imena atributa: note.put("desc2", description)
-
         // koristenje CUSTOM OBJECTA
-        val note = Note(title, description)
+        val note = Note("", title, description)
 
-
-        //db.collection().document().collection().document().set()  .... tako cu trebat
-        //mogu mijenjati ime dokumenta user1, user2, ili pomocu jedinstvenih username-a, tako sto mijenjam i formatiram stringove
-        noteRef.set(note)
+        noteBookRef.add(note)
     }
 
 
-    //Mijenja SAMO description atribut (MERGE i UPDATE)
-    private fun updateDescription() {
-        val description = mainBinding.editTextDescription.text.toString()
 
-        val note: MutableMap<String, Any> = hashMapOf()
-        note.put(KEY_DESCRIPTION, description)
-
-        //MERGE - ako vec postoji dokument, nadogradi ga, ako ne postoji stvori ga sa prosljedjenom mapom
-        noteRef.set(note, SetOptions.merge())
-
-        //moze se koristiti i UPDATE naredba - za razliku od MERGE-a , UPDATE ako vec ne postoji dokument nece ga moci stvoriti
-        //noteRef.update(note)
-        //ili
-        //noteRef.update(KEY_DESCRIPTION, description)
-    }
-
-    //Brisanje atributa description s naredbom DELETE
-    private fun deleteDescription() {
-        //1. nacin (korisnije ako se vise atributa brise, jer ih se vise moze staviti u mapu)
-        /*
-        val note: MutableMap<String, Any> = hashMapOf()
-        note.put(KEY_DESCRIPTION, FieldValue.delete())
-        noteRef.update(note)
-        */
-
-        //2. nacin - brze i korisno kada se samo jedan atribut brise
-        noteRef.update(KEY_DESCRIPTION, FieldValue.delete())
-
-        //na oba nacina se moze dodati onSuccessListener i onFailureListener, ako je potrebno
-    }
-
-
-    private fun deleteNote() {
-        noteRef.delete()
-    }
 
 
 
--- app/src/main/java/hr/ferit/nikoladanilovic/tryoutfirestorebaza/Note.kt
+++ app/src/main/java/hr/ferit/nikoladanilovic/tryoutfirestorebaza/Note.kt
@@ -1,6 +1,44 @@
 package hr.ferit.nikoladanilovic.tryoutfirestorebaza
 
-data class Note(
-    val title: String = "",
-    val description: String = ""
-)
+import com.google.firebase.firestore.Exclude
+
+class Note(
+    private var documentId : String = "",
+    private var title: String = "",
+    private var description: String = ""
+){
+    @Exclude
+    fun getDocumentId() : String {
+        return documentId
+    }
+
+    fun setDocumentId(documentId: String){
+        this.documentId = documentId
+    }
+
+    /*
+    constructor(title: String, description: String) : {
+        this.title = title
+        this.description = description
+    }
+
+     */
+
+    fun getTitle() : String {
+        return title
+    }
+
+    fun setTitle(title: String) {
+        this.title = title
+    }
+
+    fun getDescription() : String {
+        return description
+    }
+
+    fun setDescription(description: String) {
+        this.description = description
+    }
+
+
+}
